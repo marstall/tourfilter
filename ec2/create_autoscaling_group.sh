fb49923615d8d5fa4067335f56628638635e055a
marstall <marstall@gmail.com>
2012-03-04 16:18:01 -0500

    new production tag: r1

--- ec2/create_autoscaling_group.sh
+++ ec2/create_autoscaling_group.sh
@@ -2,8 +2,12 @@ echo "This will create the asg_tourfilter_$2 scaling group with $3 instances bas
 echo "press <enter> to continue."
 read line
 
-as-create-launch-config lc_tourfilter_$2 --image-id $1 --instance-type m1.small --key kp_tourfilter --group quick-start-1
+production_tag=`cat $TOURFILTER_HOME/production_tag`
+echo $production_tag
+as-create-launch-config lc_tourfilter_$2 --image-id $1 --instance-type m1.small --key kp_tourfilter --group quick-start-1 --user-data-file="user_data_file.sh r1"
 as-create-auto-scaling-group asg_tourfilter_$2 --launch-configuration lc_tourfilter_$2 --availability-zones us-east-1a --min-size $3 --max-size $3 --desired-capacity $3 --load-balancers tourfilter-lb
 as-put-notification-configuration asg_tourfilter_$2 --topic-arn arn:aws:sns:us-east-1:122872249816:tourfilter-topic --notification-types autoscaling:EC2_INSTANCE_LAUNCH, autoscaling:EC2_INSTANCE_LAUNCH_ERROR, autoscaling:EC2_INSTANCE_TERMINATE, autoscaling:EC2_INSTANCE_TERMINATE_ERROR
 as-describe-notification-configurations --auto-scaling-groups asg_tourfilter_$2
 as-describe-auto-scaling-groups asg_tourfilter_$2
+
+setup_instance_scripts.sh $2
--- ec2/deploy.sh
+++ ec2/deploy.sh
@@ -1,9 +1,29 @@
 # loops through running instances
-echo "deploy latest from repo to autoscaling group asg_tourfilter_$1?"
+echo ""
+echo "DEPLOY TOURFILTER"
+echo "Pressing enter will do a full deploy, which consists of the following:"
+echo "1. tag HEAD as $1"
+echo "2. write out tag name '$1' to $TOURFILTER_HOME/public/production_tag"
+echo "3. run <git commit -am> and <git push origin master> locally"
+echo "4. remotely perfom <git checkout $1> on all instances of autoscaling group asg_tourfilter_$2"
+echo "5. restart all apaches in autoscaling group asg_tourfilter_$2"
+echo ""
 echo "press <enter> to continue."
 read line
 
 # $1 = current autoscaling-group index (1,2,3, etc.)
+
+echo "writing tag $1 to $TOURFILTER_HOME/public/production_tag ..."
+echo $1>$TOURFILTER_HOME/public/production_tag
+
+echo "creating $TOURFILTER_HOME/ec2/user_data_file.sh ..."
+echo "git checkout $1">$TOURFILTER_HOME/ec2/user_data_file.sh
+
+echo "committing and pushing new production_tag file and git tag $1"
+git commit -am "new production tag: $1"
+git tag -a $1 -m 'tagging release $1'
+git push origin master
+
 i=0
 for instance_id in $(as-describe-auto-scaling-groups asg_tourfilter_$1 | grep INSTANCE | cut -d " " -f 3)
 do
@@ -11,9 +31,11 @@ do
 	let i+=1
 	echo "$i: deploying to $ip_address ($instance_id) ..."
 	commands_to_execute= 
-	ssh -t ec2-user@$ip_address "cd /tourfilter;git pull;sudo apachectl restart"
+	ssh -t ec2-user@$ip_address "cd /tourfilter;git checkout $1;sudo apachectl restart"
 	echo
 done
-echo "attempted to deploy to $i instances"
+
+
+echo "deployed to $i instances."
 
 
