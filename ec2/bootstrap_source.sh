4ce2cf39b064164dd323323885eb2b091e91b7ac
marstall <marstall@gmail.com>
2012-03-05 12:20:41 -0500

    new production tag: r24

--- ec2/bootstrap_source.sh
+++ ec2/bootstrap_source.sh
@@ -1,12 +1,14 @@
 #!
+
+tag= `ec2-metadata | grep user-data |  cut -d: -f2`
+cd /tourfilter
+echo $tag>bootstrap_tag
+git pull
+git checkout $tag
+rm public/index.html
+
 [[ -s "/home/ec2-user/.rvm/scripts/rvm" ]] && source "/home/ec2-user/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 rvm use ruby-1.8.7-p358
-
-tag= `ec2-metadata | grep user-data |  cut -d: -f2`
-echo tag
-cd /tourfilter
-git pull
-git checkout $tag
 bundle install
-rm public/index.html
+
