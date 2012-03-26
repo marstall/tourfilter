e7641b66bdceeb9116013640b9079767975d1741
marstall <marstall@gmail.com>
2012-03-25 23:12:14 -0400

    new production tag: r64

--- ec2/deploy.sh
+++ ec2/deploy.sh
@@ -25,8 +25,8 @@ git commit -am "new production tag: $1"
 git tag -a $1 -m 'tagging release $1'
 git push origin master --tags
 
-#$TOURFILTER_HOME/ec2/deploy_web.sh $1
-#web_failures=$? # return value
+$TOURFILTER_HOME/ec2/deploy_web.sh $1
+web_failures=$? # return value
 $TOURFILTER_HOME/ec2/deploy_daemon.sh $1
 daemon_success=$? #return value
 
