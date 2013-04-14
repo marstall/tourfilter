ca08adfcbbb4abd31bee21b7de8a075ab86120b7
Chris Marstall <chris@psychoastronomy.org>
2013-04-14 18:35:28 -0400

    testing

--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r116;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r117;sudo bundle install;sudo apachectl restart;rm public/index.html
