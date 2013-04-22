7f860ea278b78a5199d4d993b1ef688a2f8a67f6
Chris Marstall <chris@psychoastronomy.org>
2013-04-22 13:34:10 -0400

    calendar

--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r144;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r145;sudo bundle install;sudo apachectl restart;rm public/index.html
