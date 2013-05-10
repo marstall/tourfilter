6071ef074af0f33611e568e198e3621b824a429a
Chris Marstall <chris@psychoastronomy.org>
2013-05-10 14:52:21 -0400

    page_size

--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r183;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r184;sudo bundle install;sudo apachectl restart;rm public/index.html
