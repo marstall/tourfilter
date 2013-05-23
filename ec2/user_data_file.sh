36546e538c550b3160b933806c4f1807deed4738
Chris Marstall <chris@psychoastronomy.org>
2013-05-23 16:57:53 -0400

    tweak

--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r189;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r190;sudo bundle install;sudo apachectl restart;rm public/index.html
