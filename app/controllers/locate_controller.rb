db605a20d09a17c44dc8765f662173ae4ba6f4c8
Chris Marstall <chris@psychoastronomy.org>
2013-07-07 20:13:16 -0400

    locate

--- app/controllers/locate_controller.rb
+++ app/controllers/locate_controller.rb
@@ -4,6 +4,8 @@ class LocateController < ApplicationController
     set_metro_code("")
     @metro_code=nil
     @not_in_a_city=true
+    @hide_post_flyer_button=true
+    @show_logo_subhead=true
   end
   
   def set_metro_drop_down
--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r213;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r214;sudo bundle install;sudo apachectl restart;rm public/index.html
