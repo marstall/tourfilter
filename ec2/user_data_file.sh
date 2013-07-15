b4d7ed592f74215749afc38c7602d02691197b4b
Chris Marstall <chris@psychoastronomy.org>
2013-07-14 22:04:28 -0400

    post-button-tweak

--- ec2/user_data_file.sh
+++ ec2/user_data_file.sh
@@ -1,2 +1,2 @@
 #! 
-source ~/.bash_profile;cd /tourfilter;git pull;git checkout r215;sudo bundle install;sudo apachectl restart;rm public/index.html
+source ~/.bash_profile;cd /tourfilter;git pull;git checkout r216;sudo bundle install;sudo apachectl restart;rm public/index.html
