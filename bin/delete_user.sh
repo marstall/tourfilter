c5acc8f49760fd5c9ed93a11257231d39329708a
Chris Marstall <chris@psychoastronomy.org>
2014-08-07 19:29:07 -0400

    feed layout tweak

--- bin/delete_user.sh
+++ bin/delete_user.sh
@@ -5,7 +5,6 @@ if [ $do = "Y" ]
 then
  echo "OK You said yes, we're going to $action"
 m $1 "delete terms_users from terms_users,users where terms_users.user_id=users.id and (users.name='$2' or users.email_address='$2');" 
-m $1 "delete users from users where (users.name='$2' or users.email_address='$2');" 
 m $1 "delete recommendees_recommenders from recommendees_recommenders,users where recommendee_id=users.id and (users.name='$2' or users.email_address='$2');" 
 m $1 "delete recommendations from recommendations,users where recommendations.user_id=users.id and (users.name='$2' or users.email_address='$2');" 
 m $1 "delete comments from comments,users where comments.user_id=users.id and (users.name='$2' or users.email_address='$2');" 
