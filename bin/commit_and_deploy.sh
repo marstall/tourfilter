e882d9b204047c8039774dcf8ee6c83ba87c13a0
Chris Marstall <chris@psychoastronomy.org>
2014-08-20 16:38:06 -0400

    test deploy script

--- bin/commit_and_deploy.sh
+++ bin/commit_and_deploy.sh
@@ -1,5 +1,5 @@
 cd $TOURFILTER_HOME
-a=$(commit_and_tag.sh $1 | tail -1)
+a=$(commit_and_tag.sh "$1" | tail -1)
 echo "+++++++++++++++++++++++++++++++++++++++"
 echo $a
 echo "+++++++++++++++++++++++++++++++++++++++"
