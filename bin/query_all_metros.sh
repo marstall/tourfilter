7e6bd50bc03e87862b47fba6bbe1c8035e68339e
Chris Marstall <chris@psychoastronomy.org>
2014-08-22 12:47:56 -0400

    query_all_metros now just does active metros

--- bin/query_all_metros.sh
+++ bin/query_all_metros.sh
@@ -1,4 +1,4 @@
-for name in $(m shared "select code from metros order by code asc")
+for name in $(m shared "select code from metros where status='active' order by code asc")
 do
  if [ $name != "code" ]; then 
 	echo $name :
