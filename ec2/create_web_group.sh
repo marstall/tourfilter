aa283a631b311d4f76c24785cba244e942a40880
Chris Marstall <chris@psychoastronomy.org>
2014-10-23 11:40:32 -0400

    updating onsale_match

--- ec2/create_web_group.sh
+++ ec2/create_web_group.sh
@@ -6,3 +6,5 @@ as-create-auto-scaling-group asg_tourfilter_web --launch-configuration lc_tourfi
 as-put-notification-configuration asg_tourfilter_web --topic-arn arn:aws:sns:us-east-1:122872249816:tourfilter-topic --notification-types autoscaling:EC2_INSTANCE_LAUNCH, autoscaling:EC2_INSTANCE_LAUNCH_ERROR, autoscaling:EC2_INSTANCE_TERMINATE, autoscaling:EC2_INSTANCE_TERMINATE_ERROR
 as-describe-notification-configurations --auto-scaling-groups asg_tourfilter_web
 as-describe-auto-scaling-groups asg_tourfilter_web
+
+
